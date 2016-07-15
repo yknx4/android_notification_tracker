ec223710be2ef57426477d186e33f8ad97881421
Jorge Figueroa <yknx.4.b@gmail.com>
2016-07-15 14:15:49 -0500

    Allow nested attributes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   has_one :status_bar_notification, inverse_of: :notification
   has_one :notification_extra, inverse_of: :notification
+  accepts_nested_attributes_for :status_bar_notification
+  accepts_nested_attributes_for :notification_extra
 end
--- app/models/status_bar_notification.rb
+++ app/models/status_bar_notification.rb
@@ -2,5 +2,6 @@ class StatusBarNotification < ApplicationRecord
   has_one :notification, inverse_of: :status_bar_notification
   belongs_to :location, inverse_of: :status_bar_notifications
   accepts_nested_attributes_for :location
+  accepts_nested_attributes_for :notification
 
 end
